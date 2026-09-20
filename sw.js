ff31ad8e1757d63eb15c35abc1e6340c230e4385
getpedrostack <helio.mecanica@gmail.com>
2026-09-20 18:20:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
    o cache antigo e serve o app atualizado.
    ============================================================ */
 
-const CACHE_VERSION = 'pwr-v17';
+const CACHE_VERSION = 'pwr-v18';
 const CORE_ASSETS = [
   './',
   './index.html',
